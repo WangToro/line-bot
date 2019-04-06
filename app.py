ef0c3b065a70b074a1bad13a6f5af1597ec59f32
Wendy Wang <wangtoro0809@gmail.com>
2019-04-06 18:25:15 +0800

    update

--- app.py
+++ app.py
@@ -38,12 +38,13 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    r = '我看不懂你說甚麼'
+    r = '很抱歉你說甚麼'
 
     if msg == 'hi':
         r = 'hi'
-    elif msg == '你吃飯了嗎?':
+    elif msg == '你吃飯了嗎':
         r ='還沒'
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
